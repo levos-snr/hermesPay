48ef511d8c920e6bb763a41d1b5d8222db84d3f6
levos-snr <lewisodero27@gmail.com>
2023-10-10 19:50:54 +0300

    Final

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -5,13 +5,13 @@ import { initializeAuth, getReactNativePersistence } from "firebase/auth";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDdq6etQFOOgDiRexjhY3HPQHJzKN2QLds",
-  authDomain: "hermespay-app.firebaseapp.com",
-  projectId: "hermespay-app",
-  storageBucket: "hermespay-app.appspot.com",
-  messagingSenderId: "449144121441",
-  appId: "1:449144121441:web:72193e9dc44fbc75a2a220",
-  measurementId: "G-YHT3GM30YM",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
